cb20147a7454a0f68e43248cb1be9450b595913d
udkreddySomish <dushyanth@somish.com>
2021-03-02 12:02:07 +0530

    Fixed broken testcases

--- test/06_GovernanceCategoryNew.test.js
+++ test/06_GovernanceCategoryNew.test.js
@@ -40,14 +40,6 @@ contract('Configure Global Parameters', accounts => {
       snapshotId = await takeSnapshot();
       ms = await OwnedUpgradeabilityProxy.deployed();
       ms = await Master.at(ms.address);
-      let address = await ms.getLatestAddress('0x4756');
-      gv = await Governance.at(address);
-      address = await ms.getLatestAddress('0x5043');
-      pc = await ProposalCategory.at(address);
-      address = await ms.getLatestAddress('0x4d52');
-      mr = await MemberRoles.at(address);
-      tc = await TokenController.at(await ms.getLatestAddress('0x5443'));
-      marketConfig = await MarketConfig.at(await ms.getLatestAddress(toHex("MU")));
       mcr = await MarketCreationRewards.at(await ms.getLatestAddress(toHex("MC")));
       allMarkets = await AllMarkets.at(await ms.getLatestAddress(toHex("AM")));
       plotTok = await PlotusToken.deployed();
@@ -58,61 +50,12 @@ contract('Configure Global Parameters', accounts => {
 
     describe('Testing Governanace Test Cases', function() {
 
-      it('Should Update Governance to new contract', async function() {
-        let newGV = await Governance.new()
-        actionHash = encode1(
-          ['bytes2[]', 'address[]'],
-          [
-            [toHex('GV')],
-            [newGV.address]
-          ]
-        );
-        
-        await gvProposal(
-          6,
-          actionHash,
-          await MemberRoles.at(await ms.getLatestAddress(toHex('MR'))),
-          gv,
-          2,
-          0, accounts[0]
-        );
-        let proxy = await OwnedUpgradeabilityProxy.at(gv.address);
-        assert.equal(await proxy.implementation(), newGV.address);
-      });
-      
-      it('Should Not Update Market Config if zero address passed', async function() {
-        let oldImplementation = await OwnedUpgradeabilityProxy.at(marketConfig.address);
-        oldImplementation = await oldImplementation.implementation();
-        let actionHash = encode(
-          'upgradeContractImplementation(address,address)',
-          marketConfig.address,
-          0x0000000000000000000000000000000000000000
-        );
-        await gvProposal(
-          5,
-          actionHash,
-          await MemberRoles.at(await ms.getLatestAddress(toHex('MR'))),
-          gv,
-          2,
-          0, accounts[0]
-        );
-        let proxyCon = await OwnedUpgradeabilityProxy.at(marketConfig.address);
-        assert.equal(await proxyCon.implementation(), oldImplementation);
-      });
-
       it('Should Pause Market Creation', async function() {
         assert.equal(await allMarkets.marketCreationPaused(), false);
         let actionHash = encode(
           'pauseMarketCreation()'
         );
-        await gvProposal(
-          16,
-          actionHash,
-          await MemberRoles.at(await ms.getLatestAddress(toHex('MR'))),
-          gv,
-          2,
-          0
-        );
+        await allMarkets.pauseMarketCreation();
         assert.equal(await allMarkets.marketCreationPaused(), true);
       });
 
@@ -121,14 +64,8 @@ contract('Configure Global Parameters', accounts => {
         let actionHash = encode(
           'pauseMarketCreation()'
         );
-        await gvProposal(
-          16,
-          actionHash,
-          await MemberRoles.at(await ms.getLatestAddress(toHex('MR'))),
-          gv,
-          2,
-          0
-        );
+        await assertRevert(allMarkets.pauseMarketCreation());
+
         assert.equal(await allMarkets.marketCreationPaused(), true);
       });
 
@@ -137,14 +74,7 @@ contract('Configure Global Parameters', accounts => {
         let actionHash = encode(
           'resumeMarketCreation()'
         );
-        await gvProposal(
-          17,
-          actionHash,
-          await MemberRoles.at(await ms.getLatestAddress(toHex('MR'))),
-          gv,
-          2,
-          0
-        );
+        await allMarkets.resumeMarketCreation();
         assert.equal(await allMarkets.marketCreationPaused(), false);
       });
 
@@ -153,14 +83,7 @@ contract('Configure Global Parameters', accounts => {
         let actionHash = encode(
           'resumeMarketCreation()'
         );
-        await gvProposal(
-          17,
-          actionHash,
-          await MemberRoles.at(await ms.getLatestAddress(toHex('MR'))),
-          gv,
-          2,
-          0
-        );
+        await assertRevert(allMarkets.resumeMarketCreation());
         assert.equal(await allMarkets.marketCreationPaused(), false);
       });
 
@@ -170,112 +93,23 @@ contract('Configure Global Parameters', accounts => {
         await plotTok.transfer(mcr.address, 1000000000000);
         plbalPlot = await plotTok.balanceOf(mcr.address);
         let userbalPlot = await plotTok.balanceOf(newAB);
+        await mcr.transferAssets(plotTok.address, newAB, 1000000000000);
         let actionHash = encode(
           'transferAssets(address,address,uint256)',
           plotTok.address,
           newAB,
           1000000000000
         );
-        let pId = await gv.getProposalLength();
-        await gvProposal(
-          18,
-          actionHash,
-          await MemberRoles.at(await ms.getLatestAddress(toHex('MR'))),
-          gv,
-          2,
-          0
-        );
+
         let plbalPlotAfter = await plotTok.balanceOf(mcr.address);
         let userbalPlotAfter = await plotTok.balanceOf(newAB);
         assert.equal(plbalPlot/1 - plbalPlotAfter/1, 1000000000000);
         assert.equal(userbalPlotAfter/1 - userbalPlot/1, 1000000000000);
       });
 
-      it('Should not allow create a proposal in category raiseDispute directly', async function() {
-        let p = await gv.getProposalLength();
-        await gv.createProposal("proposal", "proposal", "proposal", 0);
-        await assertRevert(gv.categorizeProposal(p, 9, 0));
-      });
-      
-      it('Should Whitelist sponsor', async function() {
-
-        let actionHash = encode(
-          'whitelistSponsor(address)',
-          newAB
-        );
-        await gvProposal(
-          21,
-          actionHash,
-          await MemberRoles.at(await ms.getLatestAddress(toHex('MR'))),
-          gv,
-          2,
-          0
-        );
-        assert.equal(await ms.whitelistedSponsor(newAB), true);
-      });
-
-      it('Should create a proposal with category of no action', async function() {
-
-        let actionHash = encode(
-          null
-        );
-        let p = await gv.getProposalLength();
-        await gv.createProposal("proposal", "proposal", "proposal", 0);
-        let canClose = await gv.canCloseProposal(p);
-        assert.equal(parseFloat(canClose),0);
-        await gv.categorizeProposal(p, 27, 0);
-        await assertRevert(gv.submitProposalWithSolution(p, "proposal", "0x1234"));
-        await gv.submitProposalWithSolution(p, "proposal", actionHash);
-        await gv.submitVote(p, 1);
-        await increaseTime(604800);
-        await gv.closeProposal(p);
-        let proposal = await gv.proposal(p);
-        assert.equal(proposal[2].toNumber(), 3);
-      });
-
-      it('Should Swap AB Member', async function() {
-
-        assert.equal(await mr.checkRole(newAB, 1), false);
-        assert.equal(await mr.checkRole(ab1, 1), true);
-        let actionHash = encode(
-          'swapABMember(address,address)',
-          newAB,
-          ab1
-        );
-        await gvProposal(
-          11,
-          actionHash,
-          await MemberRoles.at(await ms.getLatestAddress(toHex('MR'))),
-          gv,
-          2,
-          0, true
-        );
-        assert.equal(await mr.checkRole(ab1, 1), false);
-        assert.equal(await mr.checkRole(newAB, 1), true);
-      });
-
-      // it('Should Swap AB Member without whitelisting proposal', async function() {
-
-      //   assert.equal(await mr.checkRole(newAB, 1), true);
-      //   assert.equal(await mr.checkRole(ab1, 1), false);
-      //   let actionHash = encode(
-      //     'swapABMember(address,address)',
-      //     ab1,
-      //     newAB
-      //   );
-      //   let proposalId = await gv.getProposalLength();
-      //   await gv.createProposalwithSolution("Add new member", "Add new member", "hash", 11, "", actionHash)
-      //   await gv.submitVote(proposalId/1, 1);
-      //   await increaseTime(604810);
-      //   await gv.closeProposal(proposalId/1);
-      //   assert.equal(await mr.checkRole(ab1, 1), true);
-      //   assert.equal(await mr.checkRole(newAB, 1), false);
-      // });
-    });
-
     after(async function () {
       await revertSnapshot(snapshotId);
     });
-
+  });
   }
 );
