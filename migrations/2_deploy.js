e5c146822d739d6154d795c0a8efd3f59834c7a1
udkreddySomish <dushyanth@somish.com>
2021-03-05 11:11:23 +0530

    Updated deployment scripts

--- migrations/2_deploy.js
+++ migrations/2_deploy.js
@@ -1,7 +1,6 @@
 const Master = artifacts.require('Master');
 const AllMarkets = artifacts.require('MockAllMarkets');
 const MarketCreationRewards = artifacts.require('MarketCreationRewards');
-const PlotusToken = artifacts.require('MockPLOT');
 const BLOT = artifacts.require('BLOT');
 const MockchainLink = artifacts.require('MockChainLinkAggregator');
 const OwnedUpgradeabilityProxy = artifacts.require('OwnedUpgradeabilityProxy');
@@ -15,11 +14,8 @@ const BN = web3.utils.BN;
 module.exports = function(deployer, network, accounts){
   deployer.then(async () => {
       
-      let deployPlotusToken = await deployer.deploy(PlotusToken, "PLOT", "PLOT", 18,accounts[0], accounts[0]);
-      deployPlotusToken.mint(accounts[0],"30000000000000000000000000");
       let mockchainLinkAggregaror = await deployer.deploy(MockchainLink);
       let ethChainlinkOracle = await deployer.deploy(EthChainlinkOracle, mockchainLinkAggregaror.address);
-      let plotusToken = await PlotusToken.at(deployPlotusToken.address);
 
       let blotToken = await deployer.deploy(BLOT);
       let masterProxy = await deployer.deploy(Master);
@@ -28,32 +24,5 @@ module.exports = function(deployer, network, accounts){
       let mcr = await deployer.deploy(MarketCreationRewards);
       let participationMining = await deployer.deploy(ParticipationMining);
       let dr = await deployer.deploy(DisputeResolution);
-      master = await Master.at(master.address);
-      let implementations = [allMarkets.address, mcr.address, blotToken.address, participationMining.address, dr.address];
-      await master.initiateMaster(implementations, deployPlotusToken.address, accounts[0], accounts[0]);
-      master = await OwnedUpgradeabilityProxy.at(master.address);
-      await master.transferProxyOwnership(accounts[0]);
-      master = await Master.at(master.address);
-      var date = Date.now();
-      date = Math.round(date/1000);
-
-      let allMarketsProxy = await OwnedUpgradeabilityProxy.at(
-        await master.getLatestAddress(web3.utils.toHex('AM'))
-      );
-
-      let mcrProxy = await OwnedUpgradeabilityProxy.at(
-        await master.getLatestAddress(web3.utils.toHex('MC'))
-      );
-      
-      allMarkets = await AllMarkets.at(allMarketsProxy.address);
-      mcr = await MarketCreationRewards.at(mcrProxy.address);
-      // await allMarkets.setAssetPlotConversionRate(plotusToken.address, 1);
-
-
-      assert.equal(await master.isInternal(allMarkets.address), true);
-      assert.equal(await master.isInternal(mcr.address), true);
-      // await mcr.initialise()
-      await plotusToken.approve(allMarkets.address, "1000000000000000000000000")
-      await allMarkets.addInitialMarketTypesAndStart(date, ethChainlinkOracle.address, ethChainlinkOracle.address);
   });
 };
---
+++ migrations/3_initiate.js
@@ -0,0 +1,56 @@
+const Master = artifacts.require('Master');
+const AllMarkets = artifacts.require('MockAllMarkets');
+const MarketCreationRewards = artifacts.require('MarketCreationRewards');
+const PlotusToken = artifacts.require('MockPLOT');
+const BLOT = artifacts.require('BLOT');
+const MockchainLink = artifacts.require('MockChainLinkAggregator');
+const OwnedUpgradeabilityProxy = artifacts.require('OwnedUpgradeabilityProxy');
+const ParticipationMining = artifacts.require('ParticipationMining');
+const DisputeResolution = artifacts.require('DisputeResolution');
+const EthChainlinkOracle = artifacts.require('EthChainlinkOracle');
+const { assert } = require("chai");
+
+module.exports = function(deployer, network, accounts){
+  deployer.then(async () => {
+    let deployPlotusToken = await deployer.deploy(PlotusToken, "PLOT", "PLOT", 18,accounts[0], accounts[0]);
+    deployPlotusToken.mint(accounts[0],"30000000000000000000000000");
+    let plotusToken = await PlotusToken.at(deployPlotusToken.address);
+    
+    let ethChainlinkOracle = await EthChainlinkOracle.deployed();
+    let blotToken = await BLOT.deployed();
+    let masterProxy = await Master.deployed();
+    let master = await OwnedUpgradeabilityProxy.deployed();
+    let allMarkets = await AllMarkets.deployed();
+    let mcr = await MarketCreationRewards.deployed();
+    let participationMining = await ParticipationMining.deployed();
+    let dr = await DisputeResolution.deployed();
+    master = await Master.at(master.address);
+    let implementations = [allMarkets.address, mcr.address, blotToken.address, participationMining.address, dr.address];
+    console.log(accounts[0])
+    await master.initiateMaster(implementations, deployPlotusToken.address, accounts[0], accounts[0]);
+    master = await OwnedUpgradeabilityProxy.at(master.address);
+    await master.transferProxyOwnership(accounts[0]);
+    master = await Master.at(master.address);
+    var date = Date.now();
+    date = Math.round(date/1000);
+
+    let allMarketsProxy = await OwnedUpgradeabilityProxy.at(
+      await master.getLatestAddress(web3.utils.toHex('AM'))
+    );
+
+    let mcrProxy = await OwnedUpgradeabilityProxy.at(
+      await master.getLatestAddress(web3.utils.toHex('MC'))
+    );
+      
+    allMarkets = await AllMarkets.at(allMarketsProxy.address);
+    mcr = await MarketCreationRewards.at(mcrProxy.address);
+    // await allMarkets.setAssetPlotConversionRate(plotusToken.address, 1);
+
+
+    assert.equal(await master.isInternal(allMarkets.address), true);
+    assert.equal(await master.isInternal(mcr.address), true);
+    // await mcr.initialise()
+    await plotusToken.approve(allMarkets.address, "1000000000000000000000000")
+    await allMarkets.addInitialMarketTypesAndStart(date, ethChainlinkOracle.address, ethChainlinkOracle.address);
+  });
+};
