efb809ab3f03e0db0031c5bd4c02eb9d83bed7d6
udkreddySomish <dushyanth@somish.com>
2021-05-11 16:11:49 +0530

    Fixed testcases

--- migrations/3_initiate.js
+++ migrations/3_initiate.js
@@ -18,7 +18,7 @@ module.exports = function(deployer, network, accounts){
     deployPlotusToken.mint(accounts[0],"30000000000000000000000000");
     let plotusToken = await PlotusToken.at(deployPlotusToken.address);
     
-    // let ethChainlinkOracle = await EthChainlinkOracle.deployed();
+    let ethChainlinkOracle = await EthChainlinkOracle.deployed();
     let bPlotToken = await BPLOT.deployed();
     let masterProxy = await Master.deployed();
     let master = await OwnedUpgradeabilityProxy.deployed();
@@ -37,29 +37,48 @@ module.exports = function(deployer, network, accounts){
     await master.transferProxyOwnership(accounts[0]);
     master = await Master.at(master.address);
 
-//     let allMarketsProxy = await OwnedUpgradeabilityProxy.at(
-//       await master.getLatestAddress(web3.utils.toHex('AM'))
-//       );
-//     var date = Date.now();
-//     date = Math.round(date/1000);
+    allMarkets = await AllMarkets.deployed();
+		cyclicMarkets = await CyclicMarkets.deployed();
+		acyclicMarkets = await AcyclicMarkets.deployed();
+		disputeResolution = await DisputeResolution.deployed();
 
-//     allMarkets = await AllMarkets.at(allMarketsProxy.address);
-//     cm = await CyclicMarkets.at(await master.getLatestAddress(web3.utils.toHex('CM')));
-//     ac = await AcyclicMarkets.at(await master.getLatestAddress(web3.utils.toHex('AC')));
-//     // await allMarkets.setAssetPlotConversionRate(plotusToken.address, 1);
+		await master.addNewContract(web3.utils.toHex("AM"),allMarkets.address);
+		await master.addNewContract(web3.utils.toHex("DR"),disputeResolution.address);
+		await master.addNewContract(web3.utils.toHex("CM"),cyclicMarkets.address);
+		await master.addNewContract(web3.utils.toHex("AC"),acyclicMarkets.address);
 
-//     assert.equal(await master.isInternal(allMarkets.address), true);
-//     await allMarkets.addAuthorizedMarketCreator(ac.address);
-//     await allMarkets.addAuthorizedMarketCreator(cm.address);
-//     await allMarkets.initializeDependencies();
-//     await plotusToken.approve(allMarkets.address, "1000000000000000000000000");
-//     await cm.whitelistMarketCreator(accounts[0]);
-//     await cm.addInitialMarketTypesAndStart(date, ethChainlinkOracle.address, ethChainlinkOracle.address);
-//     let rf = await deployer.deploy(Referral, master.address);
-//     let ul = await deployer.deploy(UserLevels, master.address);
-//     await cm.setReferralContract(rf.address);
-//     await cm.setUserLevelsContract(ul.address);
-//     await ac.setReferralContract(rf.address);
-//     await ac.setUserLevelsContract(ul.address);
+    allMarkets = await AllMarkets.at(await master.getLatestAddress(web3.utils.toHex("AM")));
+		cyclicMarkets = await CyclicMarkets.at(await master.getLatestAddress(web3.utils.toHex("CM")));
+		acyclicMarkets = await AcyclicMarkets.at(await master.getLatestAddress(web3.utils.toHex("AC")));
+		// referral = await Referral.deployed();
+		disputeResolution = await DisputeResolution.at(await master.getLatestAddress(web3.utils.toHex("DR")));
+		bPlotToken = await BPLOT.at(await master.getLatestAddress(web3.utils.toHex("BL")));
+
+    await bPlotToken.initializeDependencies();
+
+    let allMarketsProxy = await OwnedUpgradeabilityProxy.at(
+      await master.getLatestAddress(web3.utils.toHex('AM'))
+      );
+    var date = Date.now();
+    date = Math.round(date/1000);
+
+    allMarkets = await AllMarkets.at(allMarketsProxy.address);
+    cm = await CyclicMarkets.at(await master.getLatestAddress(web3.utils.toHex('CM')));
+    ac = await AcyclicMarkets.at(await master.getLatestAddress(web3.utils.toHex('AC')));
+    // await allMarkets.setAssetPlotConversionRate(plotusToken.address, 1);
+
+    assert.equal(await master.isInternal(allMarkets.address), true);
+    await allMarkets.addAuthorizedMarketCreator(ac.address);
+    await allMarkets.addAuthorizedMarketCreator(cm.address);
+    await allMarkets.initializeDependencies();
+    await plotusToken.approve(allMarkets.address, "1000000000000000000000000");
+    await cm.whitelistMarketCreator(accounts[0]);
+    await cm.addInitialMarketTypesAndStart(date, ethChainlinkOracle.address, ethChainlinkOracle.address);
+    let rf = await deployer.deploy(Referral, master.address);
+    let ul = await deployer.deploy(UserLevels, master.address);
+    await cm.setReferralContract(rf.address);
+    await cm.setUserLevelsContract(ul.address);
+    await ac.setReferralContract(rf.address);
+    await ac.setUserLevelsContract(ul.address);
   });
 };
