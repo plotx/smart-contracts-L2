b090476aee1aceb5432f39b6ec322a55a73ed281
udkreddySomish <dushyanth@somish.com>
2021-07-30 09:56:48 +0530

    Removed redundant calls to get option length

--- contracts/OptionPricing2.sol
+++ contracts/OptionPricing2.sol
@@ -30,14 +30,13 @@ contract OptionPricing2 is IOptionPricing {
      **/
     function getOptionPrice(uint _marketId, uint _currentPrice, uint _prediction, uint[] memory _marketPricingData, address _allMarketsAddress) public view returns(uint64) {
       IAllMarkets allMarkets = IAllMarkets(_allMarketsAddress); 
-            uint optionLen = allMarkets.getTotalOptions(_marketId);
       (uint[] memory _optionPricingParams,) = allMarkets.getMarketOptionPricingParams(_marketId,_prediction);
 
       // Checking if current stake in market reached minimum stake required for considering staking factor.
       if(_optionPricingParams[1] < _marketPricingData[0] || _optionPricingParams[0] == 0)
       {
 
-        return uint64(uint(100000).div(optionLen));
+        return uint64(uint(100000).div(_optionLength));
 
       } else {
         uint _value = uint(100000).mul(_optionPricingParams[0]).div(_optionPricingParams[1]);
