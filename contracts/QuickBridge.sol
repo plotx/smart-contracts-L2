6b269ba430518fa4a34995f3d04a13bd4da4adcf
smrMadhan <madhan@somish.com>
2021-08-02 11:24:11 +0530

    Audit changes

--- contracts/QuickBridge.sol
+++ contracts/QuickBridge.sol
@@ -17,9 +17,8 @@ import "./interfaces/IToken.sol";
 
 contract QuickBridge {
     
-    address[] public tokens; 
-    address public authController;
-    address public migrationController; 
+    address public  authController;
+    address public  migrationController; 
     /**
      * @dev Checks if msg.sender is authController.
      */
@@ -29,18 +28,13 @@ contract QuickBridge {
     }
     
     mapping(bytes32 => MigrationStatus) public migrationStatus;
-    mapping(address => TokenStatus) public tokenStatus;
+    mapping(address => bool) public tokenStatus;
     
     struct MigrationStatus{
         bool initiated;
         bool completed;
     }
     
-    struct TokenStatus{
-        bool whitelisted;
-        bool enabled;
-    }
-
     event MigrationAuthorised(bytes hash, address indexed to, address indexed from, uint256 value);
     event MigrationCompleted(bytes hash, address indexed to, address indexed from, uint256 value);
 
@@ -53,8 +47,8 @@ contract QuickBridge {
     /**
      * @dev Returns the hash of given params
      */    
-    function migrationHash( bytes memory _hash, address _to, address _from, uint256 _timestamp,uint256 _amount) public view returns (bytes32){
-        return  keccak256(abi.encode(_hash, _to, _from, _timestamp,_amount));
+    function migrationHash( bytes memory _hash, address _to, address _from, uint256 _timestamp,uint256 _amount,address _token) public pure returns (bytes32){
+        return  keccak256(abi.encode(_hash, _to, _from, _timestamp,_amount,_token));
     }
     
     /**
@@ -68,13 +62,14 @@ contract QuickBridge {
         uint256 _amount,
         address _token
     ) public onlyAuthorized returns (bytes32) {
-        require(tokenStatus[_token].enabled,"Token should be enabled for migration");
-        require(migrationStatus[ migrationHash(_hash, _to, _from, _timestamp,_amount)].initiated == false, "Migration already initiated");
+        bytes32 hash =  migrationHash(_hash, _to, _from, _timestamp,_amount,_token);
+        require(tokenStatus[_token],"Token should be enabled for migration");
+        require(migrationStatus[hash].initiated == false, "Migration already initiated");
         
-        migrationStatus[ migrationHash(_hash, _to, _from, _timestamp,_amount)].initiated = true;
+        migrationStatus[hash].initiated = true;
         emit MigrationAuthorised(_hash,_to, _from,_amount);
 
-        return migrationHash(_hash, _to, _from, _timestamp,_amount);        
+        return hash;        
     }    
     
    
@@ -90,13 +85,14 @@ contract QuickBridge {
         uint256 _amount,
         address _token
     ) public returns (bool){
+        bytes32 hash =  migrationHash(_hash, _to, _from, _timestamp,_amount,_token);
         require(msg.sender == migrationController, "sender is not migration controller");
-        require(tokenStatus[_token].enabled,"Token should be enabled for migration");
-        require(migrationStatus[ migrationHash(_hash, _to, _from, _timestamp,_amount)].initiated == true, "Migration not initiated");
-        require(migrationStatus[ migrationHash(_hash, _to, _from, _timestamp,_amount)].completed == false, "Migration already completed");
+        require(tokenStatus[_token],"Token should be enabled for migration");
+        require(migrationStatus[hash].initiated == true, "Migration not initiated");
+        require(migrationStatus[hash].completed == false, "Migration already completed");
         require(IToken(_token).transfer( _to, _amount));
         
-        migrationStatus[ migrationHash(_hash, _to, _from, _timestamp,_amount)].completed = true;
+        migrationStatus[hash].completed = true;
         emit MigrationCompleted(_hash,_to, _from,_amount);
 
         return true;
@@ -105,20 +101,16 @@ contract QuickBridge {
      function whitelistNewToken(address[] memory _tokens) public onlyAuthorized{
         for(uint8 i = 0; i<_tokens.length;i++){
             require(_tokens[i] != address(0),"Token should be non-zero address");
-            require(tokenStatus[_tokens[i]].whitelisted == false,"Token exists in whitelist");
+            require(tokenStatus[_tokens[i]] == false,"Token exists in whitelist");
             
-            tokens.push(_tokens[i]);
-            tokenStatus[_tokens[i]].whitelisted = true;
-            tokenStatus[_tokens[i]].enabled = true;
+            tokenStatus[_tokens[i]] = true;
         }
        
     }
     
      function updateTokenMigrationStatus(address _token) public onlyAuthorized{
         require(_token != address(0));
-        require(tokenStatus[_token].whitelisted == true,"Token not whitelisted");
-        
-        tokenStatus[_token].enabled = !tokenStatus[_token].enabled;
+        tokenStatus[_token] = !tokenStatus[_token];
     }
    
     
