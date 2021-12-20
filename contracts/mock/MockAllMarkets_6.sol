5b4e0cd9fef7a170b083fb2a56ae4b6ea99b5ec1
udkreddySomish <dushyanth@somish.com>
2021-12-20 16:02:38 +0530

    Updated testcases for ClaimAndPredict

--- contracts/mock/MockAllMarkets_6.sol
+++ contracts/mock/MockAllMarkets_6.sol
@@ -1,8 +1,8 @@
 pragma solidity 0.5.7;
 
-import "../AllPlotMarkets_6.sol";
+import "../AllPlotMarkets_8.sol";
 
-contract MockAllMarkets_6 is AllPlotMarkets_6 {
+contract MockAllMarkets_8 is AllPlotMarkets_8 {
 
     function postResultMock(uint _val, uint _marketId) external {
         _postResult(_val, _marketId);
